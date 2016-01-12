f63ee23b3516904ed90565d285de186f9626df6e
Randall McPherson <rlmcpherson@gmail.com>
2016-01-12 14:38:16 -0500

    add swig to build.sh

--- src/geometry/build.sh
+++ src/geometry/build.sh
@@ -1,2 +1,3 @@
+swig -go -cgo -c++ -intgosize 64 s2.i 
 go build -ldflags "-L/home/tudorh/usr/lib -lgflags -L/usr/lib -lz -L/usr/local/opt/openssl/lib" -gcflags "-fPIC -O -DNDEBUG -I. -DARCH_K8 -I/home/tudorh/usr/include -I/usr/include -I/usr/local/opt/openssl/include -Wno-deprecated -DS2_USE_EXACTFLOAT"  -x
  
