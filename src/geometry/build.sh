5c4612667901b770189bd400e39451a7921cdfab
Randall McPherson <rlmcpherson@gmail.com>
2016-01-12 14:35:44 -0500

    add build script for go build

---
+++ src/geometry/build.sh
@@ -0,0 +1,2 @@
+go build -ldflags "-L/home/tudorh/usr/lib -lgflags -L/usr/lib -lz -L/usr/local/opt/openssl/lib" -gcflags "-fPIC -O -DNDEBUG -I. -DARCH_K8 -I/home/tudorh/usr/include -I/usr/include -I/usr/local/opt/openssl/include -Wno-deprecated -DS2_USE_EXACTFLOAT"  -x
+ 
